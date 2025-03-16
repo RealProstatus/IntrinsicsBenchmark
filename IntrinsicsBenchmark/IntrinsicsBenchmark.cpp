d02533ae9d0373d5546e2eda6ca5e0379f4628ed
prostatus <mel.sirvania@gmail.com>
2025-03-16 12:02:10 +0300

    v2.1

--- IntrinsicsBenchmark/IntrinsicsBenchmark.cpp
+++ IntrinsicsBenchmark/IntrinsicsBenchmark.cpp
@@ -1,40 +1,99 @@
 ﻿#include<iostream>
 #include<intrin.h>
 #include<chrono>
+#include<omp.h>
+
+/*
+
+	This version of the code is optimized for an
+	architecture with the AVX2 extension, with 16 
+	vector registers YMM x0-x15
+
+*/
 
 using namespace std;
 
 const long ITERATIONS = 500000000L;
 
 int main() {
-	double a[4];
+	double a[4], b[4], c[4], d[4], e[4], f[4], g[4];
 
-	__m256d x1 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
-	__m256d x2 = _mm256_set_pd(2.0, 2.0, 2.0, 2.0);
+	//the values of these registers do not change
+	__m256d x1 = _mm256_set_pd(2.0, 2.0, 2.0, 2.0);
+	__m256d x2 = _mm256_setzero_pd();
+	__m256d x4 = _mm256_set_pd(-120.0, -60.0, -30.0, -15.0);
+	//
+	__m256d x0 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
 	__m256d x3 = _mm256_setzero_pd();
-	__m256d x4 = _mm256_setzero_pd();
-	__m256d x5 = _mm256_set_pd(-120.0, -60.0, -30.0, -15.0);
+	//
+	__m256d x5 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
+	__m256d x6 = _mm256_setzero_pd();
+	//
+	__m256d x7 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
+	__m256d x8 = _mm256_setzero_pd();
+	//
+	__m256d x9 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
+	__m256d x10 = _mm256_setzero_pd();
+	//
+	__m256d x11 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
+	__m256d x12 = _mm256_setzero_pd();
+	//
+	__m256d x13 = _mm256_set_pd(8.0, 4.0, 2.0, 1.0);
+	__m256d x14 = _mm256_setzero_pd();
+	
 
-	auto start = chrono::high_resolution_clock::now();
+	auto start = omp_get_wtime();
 #pragma omp parallel for
 	for (int j = 0; j < 100; j++) {
 #pragma omp parallel for
 		for (long i = 0L; i < ITERATIONS; i++) {
-			x4 = _mm256_fmadd_pd(x1, x2, x3);
-			x4 = _mm256_fmadd_pd(x4, x2, x3);
-			x4 = _mm256_fmadd_pd(x4, x2, x3);
-			x1 = _mm256_fmadd_pd(x4, x2, x5);
+			//
+			x3 = _mm256_fmadd_pd(x0, x1, x2);
+			x6 = _mm256_fmadd_pd(x5, x1, x2);
+			x8 = _mm256_fmadd_pd(x7, x1, x2);
+			x10 = _mm256_fmadd_pd(x9, x1, x2);
+			x12 = _mm256_fmadd_pd(x11, x1, x2);
+			x14 = _mm256_fmadd_pd(x13, x1, x2);
+			//
+			x3 = _mm256_fmadd_pd(x3, x1, x2);
+			x6 = _mm256_fmadd_pd(x6, x1, x2);
+			x8 = _mm256_fmadd_pd(x8, x1, x2);
+			x10 = _mm256_fmadd_pd(x10, x1, x2);
+			x12 = _mm256_fmadd_pd(x12, x1, x2);
+			x14 = _mm256_fmadd_pd(x14, x1, x2);
+			//
+			x3 = _mm256_fmadd_pd(x3, x1, x2);
+			x6 = _mm256_fmadd_pd(x6, x1, x2);
+			x8 = _mm256_fmadd_pd(x8, x1, x2);
+			x10 = _mm256_fmadd_pd(x10, x1, x2);
+			x12 = _mm256_fmadd_pd(x12, x1, x2);
+			x14 = _mm256_fmadd_pd(x14, x1, x2);
+			//
+			x0 = _mm256_fmadd_pd(x3, x1, x4);
+			x5 = _mm256_fmadd_pd(x6, x1, x4);
+			x7 = _mm256_fmadd_pd(x8, x1, x4);
+			x9 = _mm256_fmadd_pd(x10, x1, x4);
+			x11 = _mm256_fmadd_pd(x12, x1, x4);
+			x13 = _mm256_fmadd_pd(x14, x1, x4);
 		}
 	}
-	auto stop = chrono::high_resolution_clock::now();
+	auto stop = omp_get_wtime();
 	
-	auto res = chrono::duration_cast<chrono::milliseconds>(stop - start);
+	auto res = stop - start;
 
-	_mm256_storeu_pd(a, x4);
+	_mm256_storeu_pd(a, x3);
+	_mm256_storeu_pd(b, x3);
+	_mm256_storeu_pd(c, x3);
+	_mm256_storeu_pd(d, x3);
+	_mm256_storeu_pd(e, x3);
+	_mm256_storeu_pd(f, x3);
+	_mm256_storeu_pd(g, x3);
 	for (int i = 0; i < 4; i++) {
-		cout << a[i] << endl;
+		cout << a[i] << " " << b[i] << " " << c[i] << " " << d[i] << " " << e[i] << " " << f[i] << " " << g[i] << endl;
 	}
 
-	cout << "Time: " << res.count() << " milliseconds;" << endl;
+	unsigned long long int flop = 100ULL * ITERATIONS * 24 * 2 * 4;
+	cout << "Time: " << res << " seconds;" << endl;
+	cout << "Performance: " << flop / (res * 1.0e9) << " GFLOPS" << endl;
 	return 0;
 }
